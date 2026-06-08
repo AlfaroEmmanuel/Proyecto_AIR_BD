06fae89a6f2146ecdd7841f9c096b41b77b3fded
AlfaroEmmanuel <alfaroemma2006@gmail.com>
2026-06-07 22:46:00 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user:     process.env.DB_USER     || 'postgres',
+    user:     process.env.DB_USER     || 'Emmanuel',
     host:     process.env.DB_HOST     || 'localhost',
     database: process.env.DB_NAME     || 'prueba_proyecto_air',
-    password: process.env.DB_PASSWORD || 'Emma28072006AM.',
+    password: process.env.DB_PASSWORD || 'EMMA28072006AM.',
     port:     Number(process.env.DB_PORT) || 5430,
     max: 20,
     idleTimeoutMillis: 30000,
